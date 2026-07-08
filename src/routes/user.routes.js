8269a3cb6421dc4be647c533838c15ff35690730
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-07-08 20:20:07 +0530

    set router of controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import  { registerUser, loginUser, logOutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import  { 
+     registerUser, 
+     loginUser, 
+     logOutUser, 
+     refreshAccessToken,
+     changeCurrentUserPassword,
+     getCurrentUser, 
+     updateAccountDetails, 
+     updateUserAvatar, 
+     updateCoverImage,
+     getUserChannelProfile, 
+     getwatchHistory
+    } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verfiyJWt } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +37,21 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verfiyJWt, logOutUser)
 
 router.route("RA_token").post(refreshAccessToken)
+
+router.route("/change_password").post(verfiyJWt, changeCurrentUserPassword)
+
+router.route("/current_user").get(verfiyJWt,getCurrentUser)
+
+router.route("/update_account-detail").patch(verfiyJWt,updateAccountDetails)
+
+router.route("/update_avatar").patch(verfiyJWt, upload.single("avatar"), updateUserAvatar)
+
+router.route("/update_coverimage").patch(verfiyJWt, upload.single("coverimage"), updateCoverImage)
+
+router.route("/channel/:username").get(verfiyJWt,getUserChannelProfile)
+
+router.route("/watchHistory").get(verfiyJWt,getwatchHistory)
+
+
 
 export default router;
